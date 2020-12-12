76a1f181b495c0c3999a0f31658d1f3b193485e1
Kin Gusmão <gusmaokin@gmail.com>
2020-12-11 22:47:02 -0300

    Fix on new FD maker

--- new_fd_maker.rb
+++ new_fd_maker.rb
@@ -2,10 +2,13 @@ file = File.open("./new_domains.txt", "r")
 raw = file.read
 file.close
 domains = raw.split("\n")
+distros = %w[original one normal]
 domains.each do |domain|
-    system("mkdir ./#{domain}/real-fd")
-    system("git clone https://github.com/aibasel/downward.git ./#{domain}/real-fd")
-    system("./#{domain}/real-fd/build.py")
+    distros.each do |d|
+        system("mkdir ./#{domain}/#{d}/real-fd")
+        system("git clone https://github.com/aibasel/downward.git ./#{domain}/#{d}/real-fd")
+        system("./#{domain}/#{d}/real-fd/build.py")
+    end
 end
 system("mkdir ./real-fd")
 system("git clone https://github.com/aibasel/downward.git ./real-fd")
