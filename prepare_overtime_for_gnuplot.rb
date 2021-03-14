a159973330be15e0a4f284e7d817a41f4930d270
Kin Gusmão <gusmaokin@gmail.com>
2021-03-14 05:34:17 -0300

    fix prepare for gnuplot

--- prepare_overtime_for_gnuplot.rb
+++ prepare_overtime_for_gnuplot.rb
@@ -24,8 +24,8 @@ end
 new_csv = "\"Number of Samples\",\"10%\",\"30%\",\"50%\",\"70%\",\"100%\"\n"
 
 number_of_samples.times do |sample|
-	new_csv += "#{sample},#{csvs["10"][sample],#{csvs["30"][sample],#{csvs["50"][sample],#{csvs["70"][sample],#{csvs["100"][sample]"
-	new_csv += "\n" unless sample == number_of_samples - 1
+	new_csv += "#{sample},#{csvs["10"][sample]},#{csvs["30"][sample]},#{csvs["50"][sample]},#{csvs["70"][sample]},#{csvs["100"][sample]}"
+	new_csv = new_csv + "\n" unless sample == number_of_samples - 1
 end
 
 File.open("#{overtime_path}/overtime_accuracy.csv", 'w') { |file| file.write(new_csv) }
