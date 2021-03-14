57da9aca79ce750f976dde71da3544af2240e93d
Kin Gusmão <gusmaokin@gmail.com>
2021-03-13 22:42:09 -0300

    Update compute_overtime to support observability folders

--- compute_overtime.rb
+++ compute_overtime.rb
@@ -8,26 +8,32 @@ end
 
 overtime_path = "./overtime"
 
-overtime_accuracy = []
-counter = []
+percentages = [10, 30, 50, 70, 100]
 
-Dir.foreach(overtime_path) do |overtime_file|    
-    next if overtime_file == ".." || overtime_file == "."
-    over_file = File.open("#{overtime_path}/#{overtime_file}")
-    overtime_raw = over_file.read
-    over_file.close
+Dir.foreach(overtime_path) do |percentage|
+    next unless percentages.include?(percentage.to_i)
 
-    overtime = CSV.parse(overtime_raw)
+    overtime_accuracy = []
+    counter = []
 
-    overtime.each do |overtime_item|
-        counter[overtime_item[0]] += 1
-        overtime_accuracy[overtime_item[0]] += overtime_item[1].to_i
+    Dir.foreach("#{overtime_path}/#{percentage}") do |overtime_file|
+        next if overtime_file == ".." || overtime_file == "."
+        over_file = File.open("#{overtime_path}/#{overtime_file}")
+        overtime_raw = over_file.read
+        over_file.close
+
+        overtime = CSV.parse(overtime_raw)
+
+        overtime.each do |overtime_item|
+            counter[overtime_item[0]] += 1
+            overtime_accuracy[overtime_item[0]] += overtime_item[1].to_i
+        end
     end
-end
 
-out_csv_text = "samples,accuracy"
-overtime_accuracy.each_with_index do |oa, i|
-    out_csv_text += "\n#{i},#{overtime_accuracy[i].to_f/counter[i].to_f}"
-end
+    out_csv_text = "samples,accuracy"
+    overtime_accuracy.each_with_index do |oa, i|
+        out_csv_text += "\n#{i},#{overtime_accuracy[i].to_f/counter[i].to_f}"
+    end
 
-File.open("#{overtime_path}/overtime_accuracy.csv", 'w') { |file| file.write(out_csv_text) }
+    File.open("#{overtime_path}/#{percentage}/overtime_accuracy.csv", 'w') { |file| file.write(out_csv_text) }
+end
